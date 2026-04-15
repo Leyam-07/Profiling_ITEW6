50557bb7c612121a5db14381456d327d60ec1670
Leyam-07 <beltranliamroyce40@gmail.com>
2026-04-15 14:39:23 +0800

    Deploy current to Cloudflare

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -7,6 +7,7 @@ const api = axios.create({
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true',
   },
   withCredentials: false,
 })
